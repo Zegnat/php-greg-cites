d9a77db4945a2a01ea454d04a0e84871075d8e7b
Martijn van der Ven <martijn@vanderven.se>
2018-06-02 17:51:13 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Silly Little HTML Citations
+
+Citations are annoying to write at the best of times, even more so within HTML.
+
+This thing tries to map BibTeX records, like those obtained from Google Scholar, to nice
+copy-paste-able HTML snippets. These HTML snippets also include microformats2 [h-cite][]
+objects, so the citations can also be extracted from the HTML.
+
+[h-cite]: http://microformats.org/wiki/h-cite
---
+++ public/index.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+require '../vendor/autoload.php';
+
+use RenanBr\BibTexParser\Listener;
+use RenanBr\BibTexParser\Parser;
+use Twig\Loader\FilesystemLoader;
+use Twig\Environment as Twig;
+
+
+$loader = new FilesystemLoader('../templates');
+$twig = new Twig($loader);
+
+$templates = array_map(
+    function ($item): string {
+        return substr($item, 0, -5);
+    },
+    array_filter(
+        \scandir('../templates', \SCANDIR_SORT_ASCENDING),
+        function ($item): bool {
+            return $item[0] !== '.';
+        }
+    )
+);
+
+$post = '';
+$template = 'APA';
+if (isset($_POST['bibtex'])) {
+    $post = $_POST['bibtex'];
+    if (isset($_POST['template']) && in_array($_POST['template'], $templates)) {
+        $template = $_POST['template'];
+    }
+    $parser = new Parser();
+    $listener = new Listener();
+    $parser->addListener($listener);
+    $parser->parseString($post);
+    $out = $twig->render($template . '.html', ['articles' => $listener->export()]);
+}
+
+?><!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>BibTeX to microformats2 enhanced HTML</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <form method="post">
+      <label for="bibtex">BibTeX</label>
+      <textarea id="bibtex" name="bibtex" autofocus><?= htmlspecialchars($post, ENT_NOQUOTES) ?></textarea>
+      <label for="template">Template to use</label>
+      <select id="template" name="template">
+<?php foreach ($templates as $option) { ?>
+        <option<?= $option === $template ? ' selected' : ''?>><?= $option ?></option>
+<?php } ?>
+      </select>
+      <button type="submit">Convert</button>
+    </form>
+    <label for="snippet">HTML</label>
+    <textarea id="snippet" readonly><?= htmlspecialchars($out, ENT_NOQUOTES) ?></textarea>
+    <p>Preview</p>
+    <div id="preview"><?= $out ?? '' ?></div>
+  </body>
+</html>
